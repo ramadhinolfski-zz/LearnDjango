a8a259c1400967bf843d009c4480d652f05e4b07
Ramadhinolfski <hanafiah.ramadhan@gmail.com>
2016-05-15 20:44:43 +0700

    File awal

---
+++ form_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/ramadhan/Development/python/django/latihan1/form_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
